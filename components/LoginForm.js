f93f1385d786d9af09ec27d5db438754ecae1cb8
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-26 14:21:25 -0400

    complete LoginForm component

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,48 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.checkInputFields = this.checkInputFields.bind(this);
+    this.changeUsernameValue = this.changeUsernameValue.bind(this);
+    this.changePasswordValue = this.changePasswordValue.bind(this);
+  }
+
+  checkInputFields(event){
+    event.preventDefault()
+    if(this.state.username.length > 0 && this.state.password.length > 0) {
+      this.props.onSubmit()
+    }
+  }
+
+  changeUsernameValue(event){
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  changePasswordValue(event){
+    this.setState({
+      password: event.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.checkInputFields}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.changeUsernameValue} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.changePasswordValue} />
           </label>
         </div>
         <div>
